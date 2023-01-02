560a4d994dc89cb229262526048d19875c01e219
Viet <viet.dang@dirox.net>
2023-01-02 14:37:27 +0700

    Make the background color the coral color

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 <template>
   <main>
     <header class="fixed-top text-center mt-2">
-      <h1 class="text-red">TO-DO LIST</h1>
+      <h1 class="text-white">TO-DO LIST</h1>
     </header>
-    <section class="container-fluid m-auto">
+    <section class="container-fluid m-auto bg-red">
       <div class="row">
         <diva class="col-sm-8">
           <input type="text" placeholder="New Task" />
