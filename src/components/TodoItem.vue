1c766ca25e538d241ddf347106f0d876f2b0c29a
Viet <viet.dang@dirox.net>
2023-01-02 15:37:01 +0700

    Add some mock todo items

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li>
+    {{ label }} <span>{{ isCompleted }}</span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["label", "isCompleted"],
+};
+</script>
+
+<style></style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,31 @@
-<template>The Todo List templates here</template>
+<template>
+  The Todo List templates here
+  <ul>
+    <todo-item
+      v-for="item in todoItems"
+      :label="item.label"
+      :isCompleted="item.isCompleted"
+      :key="item.label"
+    >
+    </todo-item>
+  </ul>
+</template>
 
-<script></script>
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+  data() {
+    return {
+      todoItems: [
+        { label: "Add Item 1", isCompleted: false },
+        { label: "Add item 2", isCompleted: false },
+      ],
+    };
+  },
+  components: {
+    TodoItem,
+  },
+};
+</script>
 
 <style></style>
