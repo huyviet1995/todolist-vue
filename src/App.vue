c8d446a4d497a6cdde6c8b32a1fb41e5d36029db
Viet <viet.dang@dirox.net>
2023-01-02 14:24:06 +0700

    Adding the bootstrap library and make the to do app in the column layout direction

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 
 <template>
-  <main>
-    <header>
-      <h1>To do app</h1>
+  <main class="d-flex flex-column">
+    <header class="">
+      <h1 class="text-red">To do app</h1>
     </header>
     <section>
       <div class="inputWrapper">
@@ -14,13 +14,3 @@
   </main>
 </template>
 
-<style scoped>
-main {
-  display: flex;
-  flex-direction: column;
-}
-header {
-  line-height: 1.5;
-  width: 100%;
-}
-</style>
