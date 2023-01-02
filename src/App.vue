119e580c445567266bc44b2539fad67e3b34f6e9
Viet <viet.dang@dirox.net>
2023-01-02 14:55:16 +0700

    Increase the width of the input and the header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script setup></script>
 
 <template>
-  <main>
-    <header class="fixed-top text-center mt-2">
+  <main class="d-flex flex-column align-items-center justify-content-center">
+    <header class="text-center mb-4">
       <h1 class="text-white">TO-DO LIST</h1>
     </header>
-    <section class="container-fluid m-auto bg-red">
-      <div class="row">
-        <div class="col-sm-8">
-          <input class="py-2 px-2 h-100" type="text" placeholder="New Task" />
-        </div>
-        <div class="col-sm-4">
-          <button class="btn btn-primary ml-1 py-2 h-100">ADD</button>
-        </div>
+    <section
+      class="container-fluid m-auto bg-red w-100 d-flex align-items-center justify-content-center"
+    >
+      <div class="row m-auto">
+        <input
+          class="py-2 px-2 h-100 w-40"
+          type="text"
+          placeholder="New Task"
+        />
+        <button class="btn btn-primary ml-1 py-2 h-100">ADD</button>
       </div>
     </section>
   </main>
