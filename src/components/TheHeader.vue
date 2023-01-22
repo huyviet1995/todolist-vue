fa534f0db58814f1584777466a34ae54b4a5dd81
Viet <viet.dang@dirox.net>
2023-01-22 20:18:55 +0700

    Cursor pointer not found on item fix

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,35 +1,33 @@
 <template>
-  <v-card class="mx-auto overflow-hidden" width="344">
-    <v-app-bar color="orange-darken-4" class="v-app-bar" mb-3 prominent>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <nav>
-        <router-link to="/">
-          {{ todoPageNavLabel }}
-        </router-link>
-        <router-link v-if="!isAuthenticated" to="/auth">Login</router-link>
-        <span v-else @click="onLogout">Logout</span>
-      </nav>
-    </v-app-bar>
+  <v-app-bar color="orange-darken-4" class="v-app-bar" mb-3 prominent>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <nav>
+      <router-link to="/">
+        {{ todoPageNavLabel }}
+      </router-link>
+      <router-link v-if="!isAuthenticated" to="/auth">Login</router-link>
+      <span v-else @click="onLogout">Logout</span>
+    </nav>
+  </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <router-link to="/">{{ todoPageNavLabel }}</router-link>
-          </v-list-item>
-          <v-list-item v-if="!isAuthenticated">
-            <router-link to="/auth">Login</router-link>
-          </v-list-item>
-          <v-list-item v-else>
-            <span @click="onLogout">Logout</span>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+  <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+    <v-list nav dense>
+      <v-list-item-group
+        v-model="group"
+        active-class="deep-purple--text text--accent-4"
+      >
+        <v-list-item>
+          <router-link to="/">{{ todoPageNavLabel }}</router-link>
+        </v-list-item>
+        <v-list-item v-if="!isAuthenticated">
+          <router-link to="/auth">Login</router-link>
+        </v-list-item>
+        <v-list-item v-else>
+          <span @click="onLogout">Logout</span>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
@@ -67,17 +65,21 @@ header {
 
 nav {
   display: flex;
-  gap: 20px;
   width: fit-content;
   margin-left: auto;
   margin-right: 40px;
+  height: 100%;
+  z-index: 9999;
 }
 nav a,
 nav span {
   font-weight: bold;
   text-decoration: none;
   text-transform: uppercase;
-  cursor: pointer;
   color: white;
+  margin: auto 40px;
+  text-decoration: none;
+  cursor: pointer !important;
+  font-size: large;
 }
 </style>
