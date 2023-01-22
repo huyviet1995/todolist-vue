d24b2fed7cd6aca656314b8ad0f0b5b0897a6df2
Viet <viet.dang@dirox.net>
2023-01-22 16:08:31 +0700

    Redirect the user to login form if the user has not logged in

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/HomePage.vue";
 import UserAuth from "../components/UserAuth.vue";
+import store from "../store";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,13 +10,26 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: Home,
+      meta: { requiresAuth: true },
     },
     {
       path: "/auth",
       name: "auth",
       component: UserAuth,
+      meta: { requiresUnauth: true },
     },
   ],
+});
+
+router.beforeEach(function (to, from, next) {
+  const isAuthenticated = store.getters["auth/isAuthenticated"];
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next("/auth");
+  } else if (to.meta.requiresUnauth && isAuthenticated) {
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
